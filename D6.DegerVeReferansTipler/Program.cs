959031976c6cf5c5312956380695e2d02db485fa
BT <BT@DESKTOP-G7MDNAF>
2021-02-14 20:47:24 +0300

    +DegerVeReferansTipler

--- D6.DegerVeReferansTipler/Program.cs
+++ D6.DegerVeReferansTipler/Program.cs
@@ -6,7 +6,20 @@ namespace D6.DegerVeReferansTipler
     {
         static void Main(string[] args)
         {
-            
+            //int,decimal,float,double,bool = değer tip // Eğer başka bir değere bak denilirse ramin stack bölgesinde kopyalama yapar
+            //array,class,interface = referans tip // Eğer başka bir değere bak denilirse ramin heap bölgesinde denilen yere bakar ve önceki değerlerini ramden düşürür
+
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+
+
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+
         }
     }
 }

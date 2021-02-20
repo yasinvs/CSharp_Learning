4fad2bcce5fe8ca0c61490cedea433506fdd7fdd
yasin sekman <sekmanyasin@hotmail.com>
2021-02-20 22:19:46 +0300

    20.02.2021

---
+++ Constructions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Constructions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer() { Id = 1, FirstName = "Engin", LastName = "Demiroğ", City = "Ankara" };
+
+            Customer customer2 = new Customer(2,"Derin","Demiroğ","Ankara");
+
+        }
+    }
+
+    class Customer
+    {
+        //default Constructor
+        public Customer()
+        {
+
+        }
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            this.Id = id;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.City = city;
+
+            Console.WriteLine("Yapıcı blok çalıştı");
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ MyReferenceTypes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MyReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personal personal1 = new Personal();
+            personal1.Id = 0001;
+            personal1.FirstLastName = "Ahmet Şahin";
+
+            Customer customer1 = new Customer();
+            customer1.Id = 0002;
+            customer1.FirstLastName = "Selami Şahan";
+            customer1.CreditCardNumber = 1234567890;
+
+            Employee employee1 = new Employee();
+            employee1.Id = 0003;
+            employee1.EmployeeNumber = 0003;
+            employee1.FirstLastName = "Ramiz Karakuş";
+            employee1.Department = "IT Support";
+
+            personal1 = customer1;
+            Console.WriteLine(personal1.FirstLastName);
+            Console.WriteLine(((Customer)personal1).CreditCardNumber);
+            Console.WriteLine("////");
+            personal1 = employee1;
+            Console.WriteLine(personal1.FirstLastName);
+        }
+    }
+
+    class Personal
+    {
+        public int Id { get; set; }
+        public string FirstLastName { get; set; }
+
+    }
+
+    class Customer : Personal
+    {
+        public int CreditCardNumber { get; set; }
+
+    }
+    class Employee : Personal
+    {
+        public int EmployeeNumber { get; set; }
+        public string Department { get; set; }
+    }
+}

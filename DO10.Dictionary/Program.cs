8b843a38e1e2e2335b1da44b2255140a1f0d9ef4
yasin sekman <sekmanyasin@hotmail.com>
2021-02-21 12:16:41 +0300

    21.02.2021

---
+++ DO10.Dictionary/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DO10.Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dictCollections = new Dictionary<string, string>();
+
+            dictCollections.Add("txt", "Text File");
+            dictCollections.Add("png", "Image File");
+            dictCollections.Add("cs", "C# File");
+
+            foreach (var item in dictCollections)
+            {
+                Console.WriteLine(item.Key + " = " + item.Value);
+            }
+
+            Console.WriteLine("/// /// /// /// /// ///");
+
+            Dictionary<int, string> dictCollections1 = new Dictionary<int, string>();
+
+            dictCollections1.Add(2, "Mehmet Kar");
+            dictCollections1.Add(0, "Selam Yiğit");
+            dictCollections1.Add(1, "Arkadaş Yiğiti");
+
+            for (int i = 0; i < dictCollections1.Count; i++)
+            {
+                Console.WriteLine(i + " = " + dictCollections1[i]);
+            }
+
+            //Metotlar ve Özellikleri
+            bool varlikSorgulamasiKey = dictCollections.ContainsKey("txt");
+            Console.WriteLine(varlikSorgulamasiKey ? "Bulundu" : "Bulunamadı");
+            bool varlikSorgulamasiValue = dictCollections.ContainsValue("Text File");
+            Console.WriteLine(varlikSorgulamasiValue ? "Bulundu" : "Bulunamadı");
+
+            bool silinenDeger = dictCollections.Remove("txt");
+            Console.WriteLine(silinenDeger ? "Silindi" : "Silinemedi");
+
+            dictCollections.Clear();
+
+
+
+        }
+    }
+}
---
+++ DO11.MyDictionary/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DO11.MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> dictionary = new MyDictionary<string, string>();
+            dictionary.Add("a", "b");
+            dictionary.Add("b", "c");
+            dictionary.Add("c", "d");
+        }
+    }
+
+    class MyDictionary<K,V>
+    {
+        K[] keys;
+        V[] values;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            K[] _tempKey = keys;
+            V[] _tempValue = values;
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < _tempKey.Length; i++)
+            {
+                keys[i] = _tempKey[i];
+                values[i] = _tempValue[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+    }
+}
